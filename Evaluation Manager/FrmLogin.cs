1586e0f708f3d7beeb0f729b32946c182827bb22
foi <foi@LAB2RM16>
2025-05-08 11:33:10 +0200

    Add project files.

---
+++ Evaluation Manager/FrmLogin.cs
@@ -0,0 +1,20 @@
+namespace Evaluation_Manager
+{
+    public partial class FrmLogin : Form
+    {
+        public FrmLogin()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
